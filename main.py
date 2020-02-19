463120fcc02d52b42579c04e4191521c3eda48da
Tobi Ka <t.kaestle@vivaconagua.org>
2020-02-19 18:57:53 +0100

    stage

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from migration import Migration
 #load config file 
 def loadConfig():
     with open("conf/config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     return cfg
 
 def main():
