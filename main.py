6f16f28dba3b1f02ecdb181ab27e8caf470ad840
Tobi Ka <t.kaestle@vivaconagua.org>
2020-02-19 14:21:04 +0100

    bug

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from migration import Migration
 #load config file 
 def loadConfig():
     with open("conf/config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile)
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     return cfg
 
 def main():
